f362ddc87ac9e7cf8c7ef934ea10e1b091826151
Britton Hayes <46035482+brittonhayes@users.noreply.github.com>
2023-01-24 17:51:03 -0800

    fix: Fixes #9 (#14)
    Co-authored-by: brittonhayes <brittonhayes@users.noreply.github.com>

--- packages/inventory-ui/src/routes/[lang=lang]/+page.svelte
+++ packages/inventory-ui/src/routes/[lang=lang]/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import LL from '$i18n/i18n-svelte';
 	import TitleBar from '$lib/components/TitleBar.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
 	let pages = data.links.filter((link) => {
-		return link.label !== 'Home';
+		return link.label !== $LL.home.title();
 	});
 </script>
 
@@ -17,7 +18,7 @@
 				class="card transition-colors hover:bg-base-200/30 border-2 flex-grow rounded-none lg:min-w-[16rem] lg:min-h-[10rem] bg-base-100 shadow-lg col-span-1"
 			>
 				<div class="card-body flex justify-center items-center">
-					<h2 class="card-title text-center text-3xl">{link.label}</h2>
+					<h2 class="card-title text-center text-xl md:text-3xl">{link.label}</h2>
 				</div>
 			</a>
 		{/each}
